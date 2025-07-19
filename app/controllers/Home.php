403efe60cf7fbde3c2e3153e5bdfc0fdffede4d0
Ajik D. Jongos <ajikdjongosofficial@gmail.com>
2025-07-19 10:25:22 +0700

    feat: view update; add asset

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -3,8 +3,10 @@
 class Home extends Controller {
     public function index() {
         $data['judul'] = 'Home';
+        $data['BASEURL'] =  BASEURL;
         $this->view('templates/header', $data);
         $this->view('home/index');
+        $this->view('templates/footer');
     }
 
 }
